1097b9f99ad28ac0dcc9c7a8b783148d132101f0
Francis Xavier <njokuchimauche@gmail.com>
2020-04-08 02:35:02 +0100

    Book Entity

---
+++ src/main/java/com/example/Bookstore/entity/Book.java
@@ -0,0 +1,44 @@
+package com.example.Bookstore.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="tbl_bok")
+@Setter
+@Getter
+@ToString
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String sku;
+
+    private String name;
+
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    private boolean active;
+
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
+
+    @Column(name = "date_created")
+    private Date createdOn;
+
+    @Column(name = "last_updated")
+    private Date updatedOn;
+}
