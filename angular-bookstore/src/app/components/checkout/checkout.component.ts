dd6dcc9bae27e1e42c0d56c13205b15542b5582c
Francis Xavier <njokuchimauche@gmail.com>
2020-06-07 18:13:58 +0100

    Create checkout formgroup

---
+++ angular-bookstore/src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css']
+})
+export class CheckoutComponent implements OnInit {
+
+  checkoutFormGroup: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+
+    this.checkoutFormGroup = this._formBuilder.group({
+      customer: this._formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      })
+    })
+  }
+
+}
